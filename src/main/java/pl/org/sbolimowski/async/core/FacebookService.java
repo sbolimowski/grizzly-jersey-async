ecb2a8783426de7bb8c4601ebdb4de2ba4e0c11b
Slawomir Bolimowski <slawomir.bolimowski@allegro.pl>
2014-10-02 23:11:30 +0200

    names refactoring

--- src/main/java/pl/org/sbolimowski/async/core/FacebookService.java
+++ src/main/java/pl/org/sbolimowski/async/core/FacebookService.java
@@ -1,9 +1,10 @@
 package pl.org.sbolimowski.async.core;
 
 import org.jvnet.hk2.annotations.Service;
-import pl.org.sbolimowski.async.model.FacebookInfo;
+import pl.org.sbolimowski.async.model.FacebookUser;
 
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.InvocationCallback;
 import javax.ws.rs.client.WebTarget;
 import java.util.concurrent.Future;
 
@@ -13,12 +14,22 @@ public class FacebookService {
     private final WebTarget target = ClientBuilder.newClient()
             .target("http://graph.facebook.com/");
 
-    public Future<FacebookInfo> getInfoAsync(String user) {
+    public Future<FacebookUser> userAsync(String user) {
         return target
                 .path("/{user}")
                 .resolveTemplate("user", user)
                 .request()
                 .async()
-                .get(FacebookInfo.class);
+                .get(new InvocationCallback<FacebookUser>() {
+                    @Override
+                    public void completed(FacebookUser facebookUser) {
+
+                    }
+
+                    @Override
+                    public void failed(Throwable throwable) {
+
+                    }
+                });
     }
 }
